f9d0db39e04e3c925f06b7e06e95671f4da84563
Nakasato7423 <yasminnakasatoferri@gmail.com>
2025-08-11 11:51:18 -0300

    aula 11/08

--- index.js
+++ index.js
@@ -2,12 +2,27 @@ import express from 'express'
 
 const app = express()
 const PORT = 3000
+let items = []
+let contador = 1
 
 app.use(express.json())
 
 //ROTA DE TESTE
 app.get("/status", (req, res) => {
-  res.json({message: "API Online"})
+  res.json({message: "NIKOLA TESLA"})
+})
+
+//ENVIANDO DADOS PARA A API
+app.post("/usuarios", (req, res) => {
+const {nome} = req.body
+
+const novoItem = {
+  id: contador++,
+  nome
+}
+
+items.push(novoItem)
+res.status(201).json(novoItem)
 })
 
 app.listen(PORT, () => {
